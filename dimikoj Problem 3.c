f8792f527c8d1bafbdf88b5ff9ddae3dd78d6f89
Tasin212 <79746688+Tasin212@users.noreply.github.com>
2021-03-29 14:14:17 +0600

    Add files via upload

---
+++ dimikoj Problem 3.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=1000;i>=1;i--)
+    {
+        printf("%d\t",i);
+        i=i-1;
+        printf("%d\t",i);
+        i=i-1;
+        printf("%d\t",i);
+        i=i-1;
+        printf("%d\t",i);
+        i=i-1;
+        printf("%d\n",i);
+        
+        
+    }
+
+    return 0;
+}
